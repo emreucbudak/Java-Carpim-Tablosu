5a62c2701f08e1835dfb4c3a5f29556191e9f793
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-03-22 15:51:46 +0300

    Add files via upload

---
+++ Carpim.java
@@ -0,0 +1,19 @@
+package test;
+
+
+public class Carpim {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		for(a = 1 ; a<10 ; a++) {
+			for (b = 1 ; b<10 ; b++) {
+				c = a*b;
+				System.out.println(a + "x" + b + "=" + c);
+				
+			}
+		}
+		
+		
+	}
+
+}
